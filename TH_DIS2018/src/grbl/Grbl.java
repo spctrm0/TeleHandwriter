31782563bc0aba5f3a75046f956280923a9eae9b
spctrm0 <spctrm0@gmail.com>
2017-12-05 20:01:21 +0900

    save before dev seeking mode

--- TH_DIS2018/src/grbl/Grbl.java
+++ TH_DIS2018/src/grbl/Grbl.java
@@ -6,27 +6,33 @@ import java.util.List;
 
 public class Grbl {
 	public SerialComm	serialComm	= null;
-	
+
 	public final int	bfrSizeMx	= 128;
 
 	public int			bfrSize		= 0;
 
-	public List<String>	grblBfr	= null;
+	public List<String>	receivedMsg	= null;
+	public List<String>	grblBfr		= null;
 	public List<String>	revervedMsg	= null;
+
+	public StringBuffer	prtTxtBfr	= null;
 
 	public void setSerialComm(SerialComm _serialComm) {
 		serialComm = _serialComm;
 	}
 
 	public Grbl() {
+		receivedMsg = Collections.synchronizedList(new ArrayList<String>());
 		grblBfr = Collections.synchronizedList(new ArrayList<String>());
 		revervedMsg = Collections.synchronizedList(new ArrayList<String>());
+
+		prtTxtBfr = new StringBuffer();
 	}
 
 	public void thread() {
 		streaming();
 	}
-	
+
 	public void streaming() {
 		while (bfrSize <= bfrSizeMx && revervedMsg.size() > 0) {
 			if (bfrSize + revervedMsg.get(0).length() <= bfrSizeMx) {
@@ -41,15 +47,25 @@ public class Grbl {
 	}
 
 	public void init() {
-		bfrSize = 0;
-		grblBfr.clear();
 		revervedMsg.clear();
+		grblBfr.clear();
+		bfrSize = 0;
 	}
 
 	public void read(String _msg) {
 		if (_msg.equals("ok") || _msg.contains("error:")) {
 			bfrSize -= grblBfr.get(0).length();
 			grblBfr.remove(0);
+			prtTxtBfr.append("<MSG>").append('\t').append("Received msg...").append('\n');
+			for (int i = 0; i < receivedMsg.size(); i++) {
+				String receivedMsg_ = receivedMsg.get(i);
+				prtTxtBfr.append('\t').append(receivedMsg_);
+			}
+			System.out.println(prtTxtBfr);
+			prtTxtBfr.setLength(0);
+			receivedMsg.clear();
+		} else {
+			receivedMsg.add(_msg);
 		}
 	}
 
