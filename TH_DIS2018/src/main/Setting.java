4f37f1e94611af2f67e90d3d6058b63749a79cd6
spctrm0 <spctrm0@gmail.com>
2017-12-08 01:49:24 +0900

    Save before delete all the multi thread design

--- TH_DIS2018/src/main/Setting.java
+++ TH_DIS2018/src/main/Setting.java
@@ -1,32 +1,32 @@
 package main;
 
 public class Setting {
-	public static String	connectionChkTxt	= "Grbl 1.1f ['$' for help]";
+	public static String connectionChkTxt = "Grbl 1.1f ['$' for help]";
 
-	public static int		myPort				= 6666;
-	public static int		targetPort			= 6666;
-	public static String	targetIp			= "192.168.0.2";
+	public static int myPort = 6666;
+	public static int targetPort = 6666;
+	public static String targetIp = "192.168.0.5";
 
-	public static boolean	isXInverted			= true;
-	public static boolean	isYInverted			= false;
+	public static boolean isXInverted = true;
+	public static boolean isYInverted = false;
 
-	public static int		servoHover			= 96;
-	public static int		servoZero			= 63;
+	public static int servoHover = 96;
+	public static int servoZero = 63;
 
-	public static int		feedRateDefault		= 600000;
+	public static int feedRateDefault = 30000;
 
-	public static float		myCalibX			= 0;
-	public static float		myCalibY			= 0;
-	public static float		targetCalibX		= 0;
-	public static float		targetCalibY		= 0;
+	public static float myCalibX = 0;
+	public static float myCalibY = 0;
+	public static float targetCalibX = 0;
+	public static float targetCalibY = 0;
 
-	public static int		myTabletWidth		= 224;
-	public static int		myTabletHeight		= 140;
-	public static int		targetTabletWidth	= 224;
-	public static int		targetTabletHeight	= 140;
+	public static int myTabletWidth = 224;
+	public static int myTabletHeight = 140;
+	public static int targetTabletWidth = 224;
+	public static int targetTabletHeight = 140;
 
-	public static int		myScreenWidth		= 1920;
-	public static int		myScreenHeight		= 1080;
-	public static int		targetScreentWidth	= 1920;
-	public static int		targetScreenHeight	= 1080;
+	public static int myScreenWidth = 1920;
+	public static int myScreenHeight = 1080;
+	public static int targetScreentWidth = 1920;
+	public static int targetScreenHeight = 1080;
 }
