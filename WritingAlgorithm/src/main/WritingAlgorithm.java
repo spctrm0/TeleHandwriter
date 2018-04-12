5f010e52f43e83d44d5145deb396a187f48cae1f
spctrm0 <spctrm0@gmail.com>
2018-04-12 10:56:17 +0900

    movingaverage compare

--- WritingAlgorithm/src/main/WritingAlgorithm.java
+++ WritingAlgorithm/src/main/WritingAlgorithm.java
@@ -8,35 +8,52 @@ import processing.core.PApplet;
 import processing.core.PGraphics;
 
 public class WritingAlgorithm extends PApplet {
-	TabletInput								tabletInput;
-	ArrayList<ReceivingPoint>	writingMethods	= new ArrayList<ReceivingPoint>();
-	ArrayList<Drawing>				drawings				= new ArrayList<Drawing>();
-	boolean										overlayMode			= true;
+	TabletInput tabletInput;
+	ArrayList<ReceivingPoint> writingMethods = new ArrayList<ReceivingPoint>();
+	ArrayList<Drawing> drawings = new ArrayList<Drawing>();
+	boolean overlayMode = true;
 
 	public void settings() {
 		fullScreen(2);
 	}
 
 	public void setup() {
+		// tabletInput = new TabletInput(this, (int) (width / 4.0f), height);
+		// writingMethods.add(new MassMethod(this, (int) (width / 4.0f), height));
+		// writingMethods.add(new MassMethod(this, (int) (width / 4.0f), height));
+		// writingMethods.add(new MassMethod(this, (int) (width / 4.0f), height));
+		//
+		// drawings.add(tabletInput);
+		// for (ReceivingPoint writingMethod_ : writingMethods)
+		// drawings.add((Drawing) writingMethod_);
+		//
+		// MassMethod m;
+		// m = (MassMethod) writingMethods.get(0);
+		// m.setMass(2);
+		// m.setDamping(0.5f);
+		// m = (MassMethod) writingMethods.get(1);
+		// m.setMass(4);
+		// m.setDamping(0.75f);
+		// m = (MassMethod) writingMethods.get(2);
+		// m.setMass(8);
+		// m.setDamping(0.90f);
+
 		tabletInput = new TabletInput(this, (int) (width / 4.0f), height);
-		writingMethods.add(new MassMethod(this, (int) (width / 4.0f), height));
-		writingMethods.add(new MassMethod(this, (int) (width / 4.0f), height));
-		writingMethods.add(new MassMethod(this, (int) (width / 4.0f), height));
+		writingMethods.add(new MovingAverageMethod(this, (int) (width / 4.0f), height));
+		writingMethods.add(new MovingAverageMethod(this, (int) (width / 4.0f), height));
+		writingMethods.add(new MovingAverageMethod(this, (int) (width / 4.0f), height));
 
 		drawings.add(tabletInput);
 		for (ReceivingPoint writingMethod_ : writingMethods)
 			drawings.add((Drawing) writingMethod_);
 
-		MassMethod m;
-		m = (MassMethod) writingMethods.get(0);
-		m.setMass(2);
-		m.setDamping(0.5f);
-		m = (MassMethod) writingMethods.get(1);
-		m.setMass(4);
-		m.setDamping(0.75f);
-		m = (MassMethod) writingMethods.get(2);
-		m.setMass(10);
-		m.setDamping(0.92f);
+		MovingAverageMethod m;
+		m = (MovingAverageMethod) writingMethods.get(0);
+		m.setSampleSize(4);
+		m = (MovingAverageMethod) writingMethods.get(1);
+		m.setSampleSize(8);
+		m = (MovingAverageMethod) writingMethods.get(2);
+		m.setSampleSize(16);
 
 		for (ReceivingPoint writingMethod_ : writingMethods)
 			tabletInput.addReceivingPointObj(writingMethod_);
@@ -45,6 +62,7 @@ public class WritingAlgorithm extends PApplet {
 	public void draw() {
 		background(255);
 		image(tabletInput.getPGraphics(), 0, 0);
+
 		if (overlayMode)
 			tabletInput.getPGraphics().tint(255, 32);
 		for (int i = 0; i < writingMethods.size(); i++) {
@@ -58,18 +76,31 @@ public class WritingAlgorithm extends PApplet {
 		noFill();
 		stroke(0);
 		for (int i = 0; i < writingMethods.size(); i++)
-			line(width / (writingMethods.size() + 1) * (i + 1), 0, width / (writingMethods.size() + 1) * (i + 1), height);
+			line(width / (writingMethods.size() + 1) * (i + 1), 0, width / (writingMethods.size() + 1) * (i + 1),
+					height);
 
 		stroke(0, 255, 0);
 		line(pmouseX, pmouseY, mouseX, mouseY);
+
+		noStroke();
+		fill(0);
+		for (int i = 0; i < writingMethods.size(); i++) {
+			if (writingMethods.get(i) instanceof MassMethod) {
+				MassMethod mm = (MassMethod) writingMethods.get(i);
+				text("mass = " + mm.getMass(), width / (writingMethods.size() + 1) * (i + 1), 20);
+				text("damping = " + mm.getDamping(), width / (writingMethods.size() + 1) * (i + 1), 40);
+			} else if (writingMethods.get(i) instanceof MovingAverageMethod) {
+				MovingAverageMethod mam = (MovingAverageMethod) writingMethods.get(i);
+				text("sampleSize = " + mam.getSampleSize(), width / (writingMethods.size() + 1) * (i + 1), 20);
+			}
+		}
 	}
 
 	public void keyPressed() {
 		if (key == 'C' || key == 'c') {
 			for (Drawing drawing_ : drawings)
 				drawing_.getPGraphics().clear();
-		}
-		else if (key == 'O' || key == 'o') {
+		} else if (key == 'O' || key == 'o') {
 			overlayMode = !overlayMode;
 		}
 	}
