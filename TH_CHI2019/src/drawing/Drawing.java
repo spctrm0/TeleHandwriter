25b8c07fca1faaf09885a23f9691e812c23afac9
spctrm0 <spctrm0@gmail.com>
2018-07-09 12:49:19 +0900

    Init of CHI2019 Proj

---
+++ TH_CHI2019/src/drawing/Drawing.java
@@ -0,0 +1,50 @@
+package drawing;
+
+import java.util.ArrayList;
+
+public class Drawing {
+	private ArrayList<Stroke> strokes;
+
+	public Drawing() {
+		strokes = new ArrayList<Stroke>();
+	}
+
+	public void addStroke(int _nthPoint, int _nthStroke, int _nthPointInStroke, float _x, float _y, float _pressure,
+			float _tiltX, float _tiltY, long _evtTimeInMsec, int _type) {
+		strokes.add(
+				new Stroke(_nthPoint, _nthStroke, _nthPointInStroke, _x, _y, _pressure, _tiltX, _tiltY, _evtTimeInMsec, _type));
+	}
+
+	public void addPoint(int _nthPoint, int _nthStroke, int _nthPointInStroke, float _x, float _y, float _pressure,
+			float _tiltX, float _tiltY, long _evtTimeInMsec, int _type) {
+		Stroke lastStroke_ = getLastStroke();
+		if (lastStroke_ != null)
+			lastStroke_.addPoint(_nthPoint, _nthStroke, _nthPointInStroke, _x, _y, _pressure, _tiltX, _tiltY, _evtTimeInMsec,
+					_type);
+	}
+
+	public ArrayList<Stroke> getStrokes() {
+		return strokes;
+	}
+
+	public int getStrokesNum() {
+		return strokes.size();
+	}
+
+	public Stroke getFirstStroke() {
+		if (getStrokesNum() > 0)
+			return strokes.get(0);
+		return null;
+	}
+
+	public Stroke getLastStroke() {
+		if (getStrokesNum() > 0)
+			return strokes.get(getStrokesNum() - 1);
+		return null;
+	}
+
+	public void removeFirstStroke() {
+		if (getStrokesNum() > 0)
+			strokes.remove(0);
+	}
+}
---
+++ TH_CHI2019/src/main/Setting.java
@@ -0,0 +1,51 @@
+package main;
+
+public class Setting {
+
+	public static float	myCalibXInPx			= 0;
+	public static float	myCalibYInPx			= 0;
+	public static float	targetCalibXInPx	= 0;
+	public static float	targetCalibYInPx	= 0;
+
+	public static int		servoHover	= 255 - 32;
+	public static int		servoZero		= 127;
+	public static float	servoDelay0	= 0.025f;
+	public static float	servoDelay1	= 0.150f;
+	public static float	servoDelay2	= 0.025f;
+	public static float	servoDelay3	= 0.150f;
+
+	public static float feedrateStrokeToStoke = 60 * 250; // 1min * (mm / sec)
+
+	public static boolean	isXInverted	= false;
+	public static boolean	isYInverted	= true;
+
+	public static float	xZero	= 43.5f;
+	public static float	yZero	= 26;
+	public static float	xBack	= -20;
+
+	public static int			myPort			= 8765;
+	public static int			targetPort	= 4321;
+	public static String	targetIp		= "192.168.0.4";
+
+	// public static int myPort = 4321;
+	// public static int targetPort = 8765;
+	// public static String targetIp = "192.168.0.8";
+
+	// public static boolean isXInverted = true;
+	// public static boolean isYInverted = false;
+
+	// public static float xZero = 43;
+	// public static float yZero = 31;
+
+	public static String connectionChkTxt = "[MSG:'$H'|'$X' to unlock]";
+
+	public static int	myTabletWidthInMm				= 224;
+	public static int	myTabletHeightInMm			= 140;
+	public static int	targetTabletWidthInMm		= 224;
+	public static int	targetTabletHeightInMm	= 140;
+
+	public static int	myScreenWidthInPx				= 1920;
+	public static int	myScreenHeightInPx			= 1080;
+	public static int	targetScreentWidthInPx	= 1920;
+	public static int	targetScreenHeightInPx	= 1080;
+}
